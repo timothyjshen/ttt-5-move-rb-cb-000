ae67fcf48811fba72f86bc3dddf73c6211c64cb0
observant-communicator-1765 <timothyjshen@gmail.com>
2019-06-05 13:55:09 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,8 +11,8 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(array, index, character = "X")
-  array[index] = character
+def move(array, index, value = "X")
+  array[index] = value
 end
 
 #def move(array, input, character = "X")
